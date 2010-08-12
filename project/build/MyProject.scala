9135c980a81ae338cb04c4479791ce22748e2ab7
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-12 12:31:05 -0400

    Use SBT's cross build support for dependency declarations.

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -3,7 +3,7 @@ import sbt._
 class MyProject(info: ProjectInfo) extends DefaultWebProject(info) {
   val scalatraVersion = "2.0.0.M1"
   val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion
-  val scalate = "org.scalatra" % "scalatra-scalate_2.8.0" % scalatraVersion
+  val scalate = "org.scalatra" %% "scalatra-scalate" % scalatraVersion
   val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.24" % "test"
   val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
