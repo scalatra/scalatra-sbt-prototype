4550d718b73a8bda3443cec4b3686dea46d8d501
Ross A. Baker <baker@alumni.indiana.edu>
2010-08-12 12:27:20 -0400

    Remove staging repository.

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -7,5 +7,4 @@ class MyProject(info: ProjectInfo) extends DefaultWebProject(info) {
   val servletApi = "org.mortbay.jetty" % "servlet-api" % "2.5-20081211" % "provided"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.24" % "test"
   val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-  val scalatraMilestone = "Scalatra milestone" at "https://oss.sonatype.org/content/repositories/orgscalatra-453/"
 }
