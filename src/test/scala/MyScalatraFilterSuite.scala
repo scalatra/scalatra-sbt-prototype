6d60fb74bf8f7ab2d1b2ba89b9ba33c1be57c421
Ross A. Baker <baker@alumni.indiana.edu>
2010-10-23 12:27:22 -0400

    [GH-1] Extend ScalatraFunSuite, not ScalatraSuite.  Thanks, mathiask.

--- src/test/scala/MyScalatraFilterSuite.scala
+++ src/test/scala/MyScalatraFilterSuite.scala
@@ -4,7 +4,7 @@ import org.scalatra._
 import org.scalatra.test.scalatest._
 import org.scalatest.matchers._
 
-class MyScalatraFilterSuite extends ScalatraSuite with ShouldMatchers {
+class MyScalatraFilterSuite extends ScalatraFunSuite with ShouldMatchers {
   addFilter(classOf[MyScalatraFilter], "/*")
 
   test("GET / returns status 200") {
