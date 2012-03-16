3122be5a2ee5e4faecefb3b339b4c277ee9d3e9e
Ross A. Baker <baker@alumni.indiana.edu>
2012-03-16 00:06:06 -0400

    Regenerated from scalatra-sbt.g8.

--- src/main/scala/Scalatra.scala
+++
@@ -1,8 +0,0 @@
-import com.example.app._
-import org.scalatra._
-
-class Scalatra extends LifeCycle {
-  override def init(context: ApplicationContext) {
-    context.mount(new MyScalatraServlet, "/*")
-  }
-}
